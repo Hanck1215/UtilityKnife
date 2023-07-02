d5bd497ad430b9c4131f823eb185c8a874179b20
Hanck1215 <st11025212@gmail.com>
2023-07-02 21:05:05 +0800

    我將Calculator資料夾移至Converter資料夾了，因為我發現溫度轉換器的基本操作是由計算機實現，因此做這個舉動使其成為Converter的函式庫，況且如果函式庫沒待在Converter資料夾內，tempConverter式存取不到的。此外我新增了一個txt檔在資料夾內，用來打commit訊息的字，否則輸入太久，終端機會當掉。

--- Converter/CalculatorPackage/CalculatorClass.py
+++ Converter/CalculatorPackage/CalculatorClass.py
@@ -3,11 +3,15 @@ class Calculator() :
         return num1 + num2
 
     def sub(self, num1, num2) :
-	return num1 - num2
+	    return num1 - num2
 
     def mult(self, num1, num2) :
-	return num1*num2  
+	    return num1*num2  
 
     def div(self, num1, num2) :
-	return num1/num2
+	    return num1/num2
+    
+def getCalculator() :
+      return Calculator()
+
 
--- Converter/tempConverter.py
+++ Converter/tempConverter.py
@@ -1,6 +1,18 @@
-class tempConverter() :
-    def FahrenheitToCelsius(self, Fahrenheit) :
-        return (Fahrenheit - 32) * (5/9)
+import CalculatorPackage.CalculatorClass as CalcGenerator
 
-TC = tempConverter()
-print( TC.FahrenheitToCelsius(78) )
+class tempConverter() :
+    Calc = None
+
+    def __init__(self) :
+        self.Calc = CalcGenerator.getCalculator()
+
+    def FahrenheitToCelsius(self, Fahrenheit) :
+        return self.Calc.sub(Fahrenheit, 32) * self.Calc.div(5, 9)
+    
+tc = tempConverter() 
+print(tc.FahrenheitToCelsius(78))
+    
+
+
+
+
